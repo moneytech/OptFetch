ee400a14cebe7db376e18ed3f8e6aa510c482eb5
Elronnd <elronnd@slashem.me>
2017-05-21 01:59:24 -0400

    Add example

---
+++ example.c
@@ -0,0 +1,25 @@
+#include "optfetch.h"
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(int argc, char **argv) {
+	bool debug = false;
+	char *name;
+	float boat;
+
+	struct opttype opts[] = { {"debug", 'd', OPTTYPE_BOOL, &debug}, {"name", 'n', OPTTYPE_STRING, name}, {"boat", 'b', OPTTYPE_FLOAT, &boat}, {0, 0, 0, 0} };
+
+	int c = fetchopts(argc, argv, opts);
+
+	if (debug) {
+		printf("Did debug.\n");
+	}
+
+	printf("Hi.  My name is %s.  What's yours?\n", name);
+
+	printf("My boat is %f feet long.  How about yours?\n", boat);
+
+	printf("Looks like I have %d arguments left over.  Fancy that now!\n", c);
+
+	return 0;
+}
